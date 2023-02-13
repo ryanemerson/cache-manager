186e9b221aad8943db0b7f538d6c32b0aed906f4
Ryan Emerson <remerson@redhat.com>
2023-02-13 12:33:28 +0000

    Quote Oracle SQL columns to force lowercase

--- manager/src/test/resources/oracle/oracle-setup.sql
+++ manager/src/test/resources/oracle/oracle-setup.sql
@@ -1,16 +1,16 @@
 connect gingersnap/password@//localhost:1521/XEPDB1
-create table gingersnap.customer (id int primary key, fullname varchar(255), email varchar(255));
-create table gingersnap.car_model (id int primary key, model varchar(255), brand varchar(255));
-create table gingersnap.airline (id int primary key, iata char(2), name varchar(32));
-create table gingersnap.gate (id int primary key, name varchar(4));
+create table gingersnap.customer ("id" int primary key, "fullname" varchar(255), "email" varchar(255));
+create table gingersnap.car_model ("id" int primary key, "model" varchar(255), "brand" varchar(255));
+create table gingersnap.airline ("id" int primary key, "iata" char(2), "name" varchar(32));
+create table gingersnap.gate ("id" int primary key, "name" varchar(4));
 
 create table gingersnap.flight (
-    id int primary key,
-    name varchar(6),
-    scheduled_time timestamp,
-    airline_id int,
-    gate_id int
+    "id" int primary key,
+    "name" varchar(6),
+    "scheduled_time" timestamp,
+    "airline_id" int,
+    "gate_id" int
 );
 
-alter table gingersnap.flight add foreign key (airline_id) references gingersnap.airline(id);
-alter table gingersnap.flight add foreign key (gate_id) references gingersnap.gate(id);
+alter table gingersnap.flight add foreign key ("airline_id") references gingersnap.airline("id");
+alter table gingersnap.flight add foreign key ("gate_id") references gingersnap.gate("id");
