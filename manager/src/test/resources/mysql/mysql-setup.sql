b6fab21aff534e56cd1ca017581f4b32020639ec
Ryan Emerson <remerson@redhat.com>
2023-02-14 15:56:00 +0000

    Utilise FK column name as JSON key when resolving FK relations

--- manager/src/test/resources/mysql/mysql-setup.sql
+++ manager/src/test/resources/mysql/mysql-setup.sql
@@ -2,4 +2,18 @@ create table gingersnap.customer(id int not null, fullname varchar(255), email v
 create table gingersnap.car_model(id int not null, model varchar(255), brand varchar(255), constraint primary key (id));
 create table gingersnap.airline(id int not null, iata char(2), name varchar(32), constraint primary key (id));
 create table gingersnap.gate(id int not null, name varchar(4), constraint primary key (id));
-create table gingersnap.flight(id int not null, name varchar(6), scheduled_time time, airline_id int, gate_id int, constraint primary key (id), foreign key (airline_id) references airline(id), foreign key (gate_id) references gate (id));
+create table gingersnap.airport(id int not null, name varchar(255), constraint primary key (id));
+create table gingersnap.flight(
+    id int not null,
+    name varchar(6),
+    scheduled_time time,
+    airline_id int,
+    gate_id int,
+    origin_id int,
+    destination_id int,
+    constraint primary key (id),
+    foreign key (airline_id) references airline(id),
+    foreign key (gate_id) references gate(id),
+    foreign key (origin_id) references airport(id),
+    foreign key (destination_id) references airport(id)
+);
--- manager/src/test/resources/oracle/oracle-setup.sql
+++ manager/src/test/resources/oracle/oracle-setup.sql
@@ -3,14 +3,19 @@ create table gingersnap.customer ("id" int primary key, "fullname" varchar(255),
 create table gingersnap.car_model ("id" int primary key, "model" varchar(255), "brand" varchar(255));
 create table gingersnap.airline ("id" int primary key, "iata" char(2), "name" varchar(32));
 create table gingersnap.gate ("id" int primary key, "name" varchar(4));
+create table gingersnap.airport ("id" int primary key, "name" varchar(255));
 
 create table gingersnap.flight (
     "id" int primary key,
     "name" varchar(6),
     "scheduled_time" timestamp,
     "airline_id" int,
-    "gate_id" int
+    "gate_id" int,
+    "origin_id" int,
+    "destination_id" int
 );
 
 alter table gingersnap.flight add foreign key ("airline_id") references gingersnap.airline("id");
 alter table gingersnap.flight add foreign key ("gate_id") references gingersnap.gate("id");
+alter table gingersnap.flight add foreign key ("origin_id") references gingersnap.airport("id");
+alter table gingersnap.flight add foreign key ("destination_id") references gingersnap.airport("id");
